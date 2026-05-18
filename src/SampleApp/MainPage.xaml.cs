e8145dccb996b32a5e183080564a9a0447500037
jingliancui <ericcui@qq.com>
2026-05-18 19:45:59 +0800

    add source code

---
+++ src/SampleApp/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace SampleApp;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+
+		Loaded+=MainPage_Loaded;
+	}
+
+	private async void MainPage_Loaded(object? sender,EventArgs e)
+	{
+		await Permissions.RequestAsync<Permissions.Camera>();
+		await Permissions.RequestAsync<Permissions.Microphone>();
+	}
+}
